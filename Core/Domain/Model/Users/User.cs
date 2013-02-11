c7d563d1cde2e9d4e0f8f1e9e19b0ee8c442a82e
Brian Scaturro <scaturrob@gmail.com>
2013-02-11 07:15:43 -0500

    add BCrypt and User.HashPassword

--- Core/Domain/Model/Users/User.cs
+++ Core/Domain/Model/Users/User.cs
@@ -1,4 +1,6 @@
-﻿namespace Core.Domain.Model.Users
+﻿using DevOne.Security.Cryptography.BCrypt;
+
+namespace Core.Domain.Model.Users
 {
     /// <summary>
     /// Represents a Pomodoro user
@@ -24,5 +26,14 @@
         /// The user's password
         /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// Hash the user's password using a Blowfish cypher
+        /// </summary>
+        public void HashPassword()
+        {
+            var salt = BCryptHelper.GenerateSalt(10);
+            Password = BCryptHelper.HashPassword(Password, salt);
+        }
     }
 }
---
+++ Test.Unit/Core/Domain/Model/Users/UserTest.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain.Model.Users;
+using DevOne.Security.Cryptography.BCrypt;
+using NUnit.Framework;
+
+namespace Test.Unit.Core.Domain.Model.Users
+{
+    [TestFixture]
+    public class UserTest
+    {
+        [Test]
+        public void HashPassword_should_hash_unhashed_password()
+        {
+            var user = new User() {Password = "password"};
+            user.HashPassword();
+            Assert.IsTrue(BCryptHelper.CheckPassword("password", user.Password));
+        }
+    }
+}
