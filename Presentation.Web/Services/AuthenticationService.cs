742bd3981fb49f24b8f00214c84414562cb04a13
Brian Scaturro <scaturrob@gmail.com>
2013-02-13 11:59:09 -0500

    add authentication service and test

---
+++ Presentation.Web/Services/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Security;
+using Core.Domain.Model.Users;
+
+namespace Presentation.Web.Services
+{
+    public class AuthenticationService
+    {
+        public User User { get; set; }
+
+        public AuthenticationService(User user)
+        {
+            User = user;
+        }
+
+        public void Authenticate(HttpResponseBase response)
+        {
+            var ticket = new FormsAuthenticationTicket(1, User.Email, DateTime.Now, DateTime.Now.AddDays(4), false, string.Empty);
+            string encrypted = FormsAuthentication.Encrypt(ticket);
+            response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encrypted));
+        }
+    }
+}
---
+++ Test.Unit/Presentation.Web/Services/AuthenticationServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using Core.Domain.Model.Users;
+using Moq;
+using NUnit.Framework;
+using Presentation.Web.Services;
+
+namespace Test.Unit.Presentation.Web.Services
+{
+    [TestFixture]
+    public class AuthenticationServiceTest
+    {
+        protected User User { get; set; }
+
+        protected AuthenticationService Service { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            User = new User() { Email = "e@a.com", Name = "Brian", Password = "password" };
+
+            Service = new AuthenticationService(User);
+        }
+
+        [Test]
+        public void Constructor_should_set_user()
+        {
+            Assert.AreSame(User, Service.User);
+        }
+
+        [Test]
+        public void Authenticate_should_set_cookie()
+        {
+            var response = new Mock<HttpResponseBase>();
+            var collection = new HttpCookieCollection();
+            response.SetupGet(x => x.Cookies).Returns(collection);
+            Service.Authenticate(response.Object);
+            Assert.AreEqual(1, collection.Count);
+        }
+    }
+}
