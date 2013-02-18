fa0b9a6fc1bc8211c3fb0dfafb475be06eb2bd2f
Brian Scaturro <scaturrob@gmail.com>
2013-02-18 07:58:26 -0500

    add specflow and web driver for acceptance testing

---
+++ Test.UI.Web.Features/Contexts/RegistrationContext.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using TechTalk.SpecFlow;
+
+namespace Test.UI.Web.Features.Contexts
+{
+    [Binding]
+    public class RegistrationContext
+    {
+        protected IWebDriver Driver;
+        protected string BaseUrl;
+
+        [BeforeScenario()]
+        public void InitScenario()
+        {
+            Driver = new FirefoxDriver();
+            BaseUrl = "http://localhost:50522/";
+        }
+
+        [AfterScenario()]
+        public void TearDownScenario()
+        {
+            Driver.Quit();
+        }
+
+        [Given(@"I am on ""(.*)""")]
+        public void GivenIAmOn(string page)
+        {
+            Driver.Navigate().GoToUrl(BaseUrl + page);
+        }
+
+        [When(@"I fill out the form")]
+        public void WhenIFillOutTheForm()
+        {
+            var email = Driver.FindElement(By.Id("Email"));
+            var name = Driver.FindElement(By.Id("Name"));
+            var password = Driver.FindElement(By.Id("Password"));
+            email.SendKeys("bscaturro@gmail.com");
+            name.SendKeys("Brian Scaturro");
+            password.SendKeys("password");
+        }
+
+        [When(@"I submit it")]
+        public void WhenISubmitIt()
+        {
+            var form = Driver.FindElement(By.CssSelector("form[action='/user/register']"));
+            form.Submit();
+        }
+
+        [Then(@"A cookie should be created")]
+        public void ThenACookieShouldBeCreated()
+        {
+            var cookie = Driver.Manage().Cookies.GetCookieNamed(".ASPXAUTH");
+            Assert.IsNotNull(cookie);
+        }
+
+        [Then(@"I should be redirected to the home screen")]
+        public void ThenIShouldBeRedirectedToTheHomeScreen()
+        {
+            Assert.AreEqual(BaseUrl, Driver.Url);
+        }
+
+    }
+}
---
+++ Test.UI.Web.Features/Registration.feature
@@ -0,0 +1,10 @@
+﻿Feature: Registration
+	As a user
+	I can register for a Pomodoro Power account
+
+Scenario: Registering an unused email address
+	Given I am on "/user/register"
+	When I fill out the form
+	And I submit it
+	Then A cookie should be created
+	And I should be redirected to the home screen
